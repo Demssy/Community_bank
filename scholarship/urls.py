fd600c2702828773346b7a656b03a74aab7e7b60
Serjym <serj.moskovec@gmail.com>
2023-01-02 08:21:38 +0200

    Merge branch 'main' of github.com:Serjym/Community_bank

--- scholarship/urls.py
+++ scholarship/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib import admin
-
 from scholarship import views
 
 urlpatterns = [
     # app scholarship
-    path('', views.scholarship, name = 'scholarship')
+    path('scolarships/', views.scholarship, name = 'scholarship'),
+    path('scolarships/create/', views.create_scolarship, name = 'create_scolarship'),
+    path('scholarships/<int:scholarship_id>/delete/', views.delete_scholarship, name='delete_scholarship'),
+    path('scholarships/all/', views.all_scholarship, name='all_scholarship'),
 
 ]
