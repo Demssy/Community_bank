19b92388c695747dd65e1b48df43f9583a6f152b
Serjym <serj.moskovec@gmail.com>
2023-01-04 22:33:17 +0200

    small fixes

--- app/forms.py
+++ app/forms.py
@@ -18,8 +18,8 @@ class UserSetting(forms.ModelForm):
     class Meta:
         model = CustomUser
         fields = ['major', 'college', 'first_name', 'last_name', 'email', 'user_avatar', 'date_of_birth', 'gender', 'bio']
-
-class DonationsForm(forms.ModelForm):
-    class Meta:
-        model = DonationsModel
-        fields = ["amount", "scholarship", "reason", "email", "message"]
+        widgets = {'date_of_birth':forms.DateInput(format=('%Y-%m-%d'),
+        attrs={'class': 'form-control', 
+               'placeholder': 'Select a date',
+               'type': 'date'
+              })}
